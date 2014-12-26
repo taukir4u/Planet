6886306757281f4b94d3af5e68232118c3d958f2
taukir4u <taukir4u@gmail.com>
2014-12-26 20:32:25 +0600

    C#
    Signed-off-by: taukir4u <taukir4u@gmail.com>

---
+++ Planet/PlanetAppUI.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Planet
+{
+    partial class PlanetAppUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.planetNameComboBox = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.showButton);
+            this.groupBox1.Controls.Add(this.planetNameComboBox);
+            this.groupBox1.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.DarkCyan;
+            this.groupBox1.Location = new System.Drawing.Point(21, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(244, 211);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Planet List";
+            // 
+            // showButton
+            // 
+            this.showButton.ForeColor = System.Drawing.Color.Indigo;
+            this.showButton.Location = new System.Drawing.Point(71, 135);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(114, 23);
+            this.showButton.TabIndex = 1;
+            this.showButton.Text = "Show Planet";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // planetNameComboBox
+            // 
+            this.planetNameComboBox.BackColor = System.Drawing.SystemColors.Info;
+            this.planetNameComboBox.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.planetNameComboBox.FormattingEnabled = true;
+            this.planetNameComboBox.Items.AddRange(new object[] {
+            "Mercury",
+            "Venus",
+            "Earth",
+            "Mars",
+            "Jupiter",
+            "Saturn",
+            "Uranus",
+            "Neptune",
+            "Pluto"});
+            this.planetNameComboBox.Location = new System.Drawing.Point(31, 54);
+            this.planetNameComboBox.Name = "planetNameComboBox";
+            this.planetNameComboBox.Size = new System.Drawing.Size(195, 23);
+            this.planetNameComboBox.TabIndex = 0;
+            // 
+            // PlanetAppUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 270);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "PlanetAppUI";
+            this.Text = "PlanetUI";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.ComboBox planetNameComboBox;
+    }
+}
+
---
+++ Planet/PlanetAppUI.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Planet
+{
+    public partial class PlanetAppUI : Form
+    {
+        public PlanetAppUI()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            string planetName=planetNameComboBox.Text;
+            
+            if(planetName=="Mercury")
+            {
+                MessageBox.Show("Mercury\nIt is in 1st position");
+            }
+
+            else if(planetName=="Venus")
+            {
+                MessageBox.Show("Venus\nIt is in 2nd position");
+            }
+
+            else if(planetName=="Earth")
+            {
+                MessageBox.Show("Earth\nIt is in 3rd position");
+            }
+                
+            else if(planetName=="Mars")
+            {
+                MessageBox.Show("Mars\nIt is in 4th position");
+            }
+            
+            else if(planetName=="Jupiter")
+            {
+                MessageBox.Show("Jupiter\nIt is in 5th position");
+            }
+            
+            else if(planetName=="Saturn")
+            {
+                MessageBox.Show("Saturn\nIt is in 6th position");  
+            }
+
+            else if(planetName=="Uranus")
+            {
+                MessageBox.Show("Uranus\nIt is in 7th position");
+            }
+ 
+            else if(planetName=="Neptune")
+            {
+                MessageBox.Show("Neptune\nIt is in 8th position");
+            }
+            
+            else if(planetName=="Pluto")
+            {
+                MessageBox.Show("Pluto\nIt is in 9th position");
+            }
+            
+            else
+            {
+                MessageBox.Show("I don’t know");
+            }
+        }
+    }
+}
